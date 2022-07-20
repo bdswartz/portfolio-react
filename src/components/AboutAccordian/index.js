31e366d7751d695f46e1d083eeb5abb8dcbbedc8
Brian Swartz <bdswartz72@gmail.com>
2022-07-19 21:31:50 -0400

    changing the tech skills sections to reflect the resume

--- src/components/AboutAccordian/index.js
+++ src/components/AboutAccordian/index.js
@@ -84,39 +84,41 @@ export default function AboutAccordian() {
                         <li>Node.JS</li>
                         <li>Express.JS</li>
                         <li>Express-Sessions</li>
-                        <li>Server-Side APIs</li>
+                        <li>REST APIs</li>
+                        <li>GraphQL APIs</li>
                         <li>mySQL</li>  
-                        <li>MongoDB</li>
-                        <li>GraphQL</li>
+                        <li>MongoDB (NoSQL)</li>
                         <li>Sequelize ORM</li>
                         <li>Mongoose ODM</li>
-                        <li>JWT Authentication</li>
                         <li>Handlebars</li>
                         <li>bcrypt</li>
                         <li>Heroku</li>
-                        <li>MVC Architecture</li>
                       </ul>
                       <div className="divider"></div>
                       <h5 className="tech-title">Front-End Stack</h5>
                       <ul className={`${twoColumns ? "tech-list-group2" : "tech-list-group3"}`}>
                         <li>JavaScript/ES6</li>
                         <li>React</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>DOM Document API</li>  
-                        <li>Third Party APIs</li>
+                        <li>Material UI</li>
+                        <li>HTML 5</li>
+                        <li>CSS 3</li>
+                        <li>DOM Document API</li>
+                        <li>GraphQL - Apollo Client</li>
                         <li>moment.js</li>
                         <li>jQuery</li>
                         <li>Bootstrap</li>
                         <li>Materialize</li>
-                        <li>Material UI</li>
                       </ul>
                       <div className="divider"></div>
-                      <h5 className="tech-title">Version Control</h5>
+                      <h5 className="tech-title">Other Skills and Technologies</h5>
                       <ul className="tech-list-group3">
-                        <li>Git</li>
+                        <li>Git Version Control</li>
                         <li>GitHub</li>
-                        <li>GitLab</li>
+                        <li>NPM</li>
+                        <li>Object-Oriented Programming</li>
+                        <li>MVC Architecture</li>
+                        <li>JWT Authentication</li>
+                        <li>State Management</li>
                       </ul>
         </AccordionDetails>
       </Accordion>
